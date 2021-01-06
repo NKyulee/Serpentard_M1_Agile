a48be2007e92ecfbe15c8f8cbb4922835806fb07
Yuli Ng <yunguyen94@gmail.com>
2021-01-06 11:50:47 +0100

    hotfix color footer link Hornela

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,29 +12,49 @@ const useStyles = makeStyles((theme) => ({
 		color: theme.palette.text.secondary,
 		backgroundColor: '#3f51b5',
 		borderRadius: 0
+	},
+	link: {
+		color: '#fff'
 	}
 }));
 
 export default function Footer() {
 	const classes = useStyles();
 
-    return(
-        <div className={classes.root}>
-            <Grid container spacing={0}>
-                <Grid item xs={4}>
-                   <Paper className={classes.paper}><a href="https://docs.google.com/forms/d/1PBKcix4n69oMGIMELuRSZFX5QsDtWPKIMHZeasvc_4Y/edit">F.A.Q</a></Paper>
-                </Grid>
-                <Grid item xs={4}>
-                   <Paper className={classes.paper}><a href="https://www.cnil.fr/fr/rgpd-exemples-de-mentions-dinformation">Mentions légales</a></Paper>
-                </Grid>
-                <Grid item xs={4}>
-                    <Paper className={classes.paper}><a href="mailto:eventinparis@yahoo.fr">Nous contacter</a></Paper>
-                </Grid>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>MSD - Copyright 2021</Paper>
-                </Grid>
-            </Grid>         
-        </div>
-    );
+	return (
+		<div className={classes.root}>
+			<Grid container spacing={0}>
+				<Grid item xs={4}>
+					<Paper className={classes.paper}>
+						<a
+							className={classes.link}
+							href="https://docs.google.com/forms/d/1PBKcix4n69oMGIMELuRSZFX5QsDtWPKIMHZeasvc_4Y/edit"
+						>
+							F.A.Q
+						</a>
+					</Paper>
+				</Grid>
+				<Grid item xs={4}>
+					<Paper className={classes.paper}>
+						<a
+							className={classes.link}
+							href="https://www.cnil.fr/fr/rgpd-exemples-de-mentions-dinformation"
+						>
+							Mentions légales
+						</a>
+					</Paper>
+				</Grid>
+				<Grid item xs={4}>
+					<Paper className={classes.paper}>
+						<a className={classes.link} href="mailto:eventinparis@yahoo.fr">
+							Nous contacter
+						</a>
+					</Paper>
+				</Grid>
+				<Grid item xs={12}>
+					<Paper className={classes.paper}>MSD - Copyright 2021</Paper>
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
-
